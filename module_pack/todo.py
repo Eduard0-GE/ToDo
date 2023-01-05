e6518b46fe42d1106650b519fdf96b86bb2510a6
Eduard0-GE <gersonabes@gmail.com>
2023-01-05 20:35:31 -0300

    ajustei a forma que a lista de tarefas aparece na janela de seleção

--- module_pack/todo.py
+++ module_pack/todo.py
@@ -6,10 +6,9 @@ class ToDoDialog(QDialog):
         super().__init__()
         self.path = f'/home/heero/Documentos/projetos/python/newtodo/task_lists/task1.json'
         self.setWindowTitle('archiveName')
-        self.setGeometry(200, 200, 300, 200)
         self.dict = {}
         self.taskDict()
-        self.ToDoLayout()
+        self.windowLayout()        
 
     def taskDict(self):
         with open(self.path, 'r') as jFile:
@@ -19,31 +18,37 @@ class ToDoDialog(QDialog):
                 self.dict[key].append(QLineEdit())
     
     def newTask(self):
-        self.dict[f'task{len(self.dict)+1}'] = ['', False, QCheckBox(), QLineEdit()]
-        self.todoLayout.addWidget(self.dict[f'task{len(self.dict)}'][2], len(self.dict), 0)
-        self.todoLayout.addWidget(self.dict[f'task{len(self.dict)}'][3], len(self.dict), 1, 1, 3)
-    
+        if len(self.dict) < 10: 
+            self.dict[f'task{len(self.dict)+1}'] = ['', False, QCheckBox(), QLineEdit()]
+            self.todoLayout.addWidget(self.dict[f'task{len(self.dict)}'][2], len(self.dict), 0)
+            self.todoLayout.addWidget(self.dict[f'task{len(self.dict)}'][3], len(self.dict), 1, 1, 3)
+        else:
+            pass
+            
     def saveToDo(self):
         #Runs trough the dict, changes the values and excludes the functions
         for key in self.dict:
-            self.dict[key][0] = self.dict[key][3].text()
-            self.dict[key][1] = self.dict[key][2].isChecked()
-            del self.dict[key][2]
-            del self.dict[key][2]
+            if self.dict[key][3].text() == "":
+                del self.dict[key]
+            else:
+                self.dict[key][0] = self.dict[key][3].text()
+                self.dict[key][1] = self.dict[key][2].isChecked()
+                del self.dict[key][2]
+                del self.dict[key][2]
         with open(self.path, 'w') as jFile:
             jFile.write(json.dumps(self.dict))
         
-    def ToDoLayout(self):
-        self.todoLayout = QGridLayout()
+    def taskLayout(self, position = 0):
         for key in self.dict:
             #Adding lineedit
             self.dict[key][3].setText(self.dict[key][0])
-            self.todoLayout.addWidget(self.dict[key][3], int(key[4]), 1, 1, 3)
+            self.todoLayout.addWidget(self.dict[key][3], int(key[4]) - position, 1, 1, 3)
             
             #Adding checkbox
             self.dict[key][2].setChecked(self.dict[key][1])
-            self.todoLayout.addWidget(self.dict[key][2], int(key[4]), 0)
+            self.todoLayout.addWidget(self.dict[key][2], int(key[4]) - position, 0)
 
+    def ButtonLayout(self):
         self.add_todo = QPushButton('New')
         self.add_todo.clicked.connect(self.newTask)
         
@@ -60,7 +65,16 @@ class ToDoDialog(QDialog):
         self.todoLayout.addWidget(self.save_todo, 0, 1)
         self.todoLayout.addWidget(self.cancel_todo, 0, 2)
         self.todoLayout.addWidget(self.delete_todo, 0, 3)        
-                       
+    
+    def windowLayout(self):
+        self.todoLayout = QGridLayout()
+        if __name__ == '__main__':   
+            self.setGeometry(200, 200, 300, 200)
+            self.ButtonLayout()
+            self.taskLayout()
+        else:
+            self.setGeometry(200, 200, 100, 100)
+            self.taskLayout(1)
         self.setLayout(self.todoLayout)
 
 class taskObject():
